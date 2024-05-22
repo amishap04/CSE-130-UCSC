b3fae86d0dceb5d50617293520ee9953ef0c138b
Amisha <aprasa14@ucsc.edu>
2024-05-22 03:16:44 +0000

    final queue

--- asgn3/queue.c
+++ asgn3/queue.c
@@ -12,43 +12,37 @@ struct queue {
     void **buf;
     sem_t full_slots;
     sem_t empty_slots;
-    sem_t mutex;
+    pthread_mutex_t mutex;
 };
 
 queue_t *queue_new(int size) {
-    queue_t *q = (queue_t *) malloc(sizeof(queue_t));
+    queue_t *q = (queue_t *)malloc(sizeof(queue_t));
     if (q) {
         q->size = size;
         q->in = 0;
         q->out = 0;
-        q->buf = (void **) malloc(size * sizeof(void *));
+        q->buf = (void **)malloc(size * sizeof(void *));
         if (!q->buf) {
             free(q);
-            q = NULL;
-        } else {
-            int rc = 0;
-            rc = sem_init(&(q->full_slots), 0, size);
-            assert(!rc);
-            rc = sem_init(&(q->empty_slots), 0, 0);
-            assert(!rc);
-            rc = sem_init(&(q->mutex), 0, 1);
-            assert(!rc);
+            return NULL;
+        }
+        if (sem_init(&q->full_slots, 0, size) != 0 ||
+            sem_init(&q->empty_slots, 0, 0) != 0 ||
+            pthread_mutex_init(&q->mutex, NULL) != 0) {
+            free(q->buf);
+            free(q);
+            return NULL;
         }
     }
     return q;
 }
 
 void queue_delete(queue_t **q) {
-    if (*q && (*q)->buf) {
-        int rc = 0;
-        rc = sem_destroy(&(*q)->full_slots);
-        assert(!rc);
-        rc = sem_destroy(&(*q)->empty_slots);
-        assert(!rc);
-        rc = sem_destroy(&(*q)->mutex);
-        assert(!rc);
+    if (q && *q) {
+        sem_destroy(&(*q)->full_slots);
+        sem_destroy(&(*q)->empty_slots);
+        pthread_mutex_destroy(&(*q)->mutex);
         free((*q)->buf);
-        (*q)->buf = NULL;
         free(*q);
         *q = NULL;
     }
@@ -56,26 +50,27 @@ void queue_delete(queue_t **q) {
 
 bool queue_push(queue_t *q, void *elem) {
     if (q == NULL) {
-        return (false);
+        return false;
     }
-    sem_wait(&(q->full_slots));
-    sem_wait(&(q->mutex));
+    sem_wait(&q->full_slots);
+    pthread_mutex_lock(&q->mutex);
     q->buf[q->in] = elem;
-    q->in = (q->in + 1) % (q->size);
-    sem_post(&(q->mutex));
-    sem_post(&(q->empty_slots));
-    return (true);
+    q->in = (q->in + 1) % q->size;
+    pthread_mutex_unlock(&q->mutex);
+    sem_post(&q->empty_slots);
+    return true;
 }
 
 bool queue_pop(queue_t *q, void **elem) {
     if (q == NULL) {
-        return (false);
+        return false;
     }
-    sem_wait(&(q->empty_slots));
-    sem_wait(&(q->mutex));
+    sem_wait(&q->empty_slots);
+    pthread_mutex_lock(&q->mutex);
     *elem = q->buf[q->out];
-    q->out = (q->out + 1) % (q->size);
-    sem_post(&(q->mutex));
-    sem_post(&(q->full_slots));
-    return (true);
+    q->out = (q->out + 1) % q->size;
+    pthread_mutex_unlock(&q->mutex);
+    sem_post(&q->full_slots);
+    return true;
 }
+
